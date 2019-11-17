ed40d9c3fc87d3f329f966ff9805f61ef09a3d4f
Vinit <vinitsarode5@gmail.com>
2019-11-17 15:12:48 -0500

    A3C network architecture updated

--- a3c/model.py
+++ a3c/model.py
@@ -1,26 +1,27 @@
 import math
 import torch
+import torch.nn as nn
 from torch.distributions import Normal
 
 class Network(torch.nn.Module):
-	def __init__(self, state_dim, action_dim, hidden_size=200, std=0.0):
+	def __init__(self, num_inputs, num_outputs, hidden_size=[256,256]):
 		super(Network, self).__init__()
-		self.state_dim = state_dim
-		self.action_dim = action_dim
-		self.hidden_size = hidden_size
-
-		self.critic = torch.nn.Sequential(
-			torch.nn.Linear(self.state_dim, hidden_size),
-			torch.nn.ReLU(),
-			torch.nn.Linear(hidden_size, 1)
+		self.critic = nn.Sequential(
+			nn.Linear(num_inputs, hidden_size[0]),
+			nn.ReLU(),
+			nn.Linear(hidden_size[0], hidden_size[1]),
+			nn.ReLU(),
+			nn.Linear(hidden_size[1], 1)
 		)
 		
-		self.actor = torch.nn.Sequential(
-			torch.nn.Linear(self.state_dim, hidden_size),
-			torch.nn.ReLU(),
-			torch.nn.Linear(hidden_size, self.action_dim),
+		self.actor = nn.Sequential(
+			nn.Linear(num_inputs, hidden_size[0]),
+			nn.ReLU(),
+			nn.Linear(hidden_size[0], hidden_size[1]),
+			nn.ReLU()
 		)
-		self.log_std = torch.nn.Parameter(torch.ones(self.action_dim) * std)
+		self.mu = nn.Linear(hidden_size[1], num_outputs)
+		self.sigma = nn.Sequential(nn.Linear(hidden_size[1], num_outputs), nn.Sigmoid())
 		self.apply(self.init_weights)
 
 	@staticmethod
@@ -30,12 +31,12 @@ class Network(torch.nn.Module):
 			torch.nn.init.constant_(m.bias, 0.1)
 
 	def forward(self, state):
-		mu = self.actor(state)
-		mu = torch.tanh(mu)
-		sigma = self.log_std.exp().expand_as(mu)
-		values = self.critic(state)
+		value = self.critic(state)
+		hidden_state = self.actor(state)
+		mu 	  = torch.tanh(self.mu(hidden_state))
+		sigma = self.sigma(hidden_state) + 0.001
 		dist  = Normal(mu, sigma)
-		return dist, values
+		return dist, value
 
 	def loss_function(self, state, action, values_t):
 		self.train()
