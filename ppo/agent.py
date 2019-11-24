0ed7e1c2ae24872d1e9602207bfb9db26b93a5c7
vinits5 <vinitsarode5@gmail.com>
2019-11-23 20:10:13 -0500

    extra tensorboard log added

--- ppo/agent.py
+++ ppo/agent.py
@@ -29,6 +29,10 @@ def ppo_iter(mini_batch_size, states, actions, log_probs, returns, advantage):
 
 def ppo_update(model, optimizer, ppo_epochs, mini_batch_size, states, actions, log_probs, returns, advantages, writer, frame_idx, clip_param=0.2):
 	total_loss = 0.0
+	# total_advantage = 0.0
+	total_actor_loss = 0.0
+	total_critic_loss = 0.0
+	total_entropy = 0.0
 	for _ in range(ppo_epochs):
 		for state, action, old_log_probs, return_, advantage in ppo_iter(mini_batch_size, states, actions, log_probs, returns, advantages):
 			dist, value = model(state)
@@ -44,10 +48,26 @@ def ppo_update(model, optimizer, ppo_epochs, mini_batch_size, states, actions, l
 
 			loss = 0.5 * critic_loss + actor_loss - 0.001 * entropy
 			total_loss += loss.item()
+			total_actor_loss += actor_loss.item()
+			total_critic_loss += critic_loss.item()
+			total_entropy += entropy.item()
 
 			optimizer.zero_grad()
 			loss.backward()
 			optimizer.step()
 	total_loss = total_loss/(ppo_epochs*1.0)
 	total_loss = total_loss/(states.size(0)/mini_batch_size)
+
+	total_critic_loss = total_critic_loss/(ppo_epochs*1.0)
+	total_critic_loss = total_critic_loss/(states.size(0)/mini_batch_size)
+
+	total_actor_loss = total_actor_loss/(ppo_epochs*1.0)
+	total_actor_loss = total_actor_loss/(states.size(0)/mini_batch_size)
+
+	total_entropy = total_entropy/(ppo_epochs*1.0)
+	total_entropy = total_entropy/(states.size(0)/mini_batch_size)
+
 	writer.add_scalar('loss/epoch', total_loss, frame_idx)
+	writer.add_scaler('critic_loss/epoch',total_critic_loss,frame_idx)
+	writer.add_scaler('actor_loss/epoch',total_actor_loss,frame_idx)
+	writer.add_scalar('entropy/epoch',total_entropy,frame_idx)
